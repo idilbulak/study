9b19e6cb3a0a8820a2f831b323a81bcfa00936c1
Idil Bulak <ibulak@f1r2s5.codam.nl>
2023-04-12 21:02:13 +0200

    ?

---
+++ srcs/requirements/wordpress/tools/www.conf.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+sed -i "s|listen = 127.0.0.1:9000|listen = 9000|g" /etc/php8/php-fpm.d/www.conf
+sed -i "s|;listen.owner = nobody|listen.owner = nobody|g" /etc/php8/php-fpm.d/www.conf
+sed -i "s|;listen.group = nobody|listen.group = nobody|g" /etc/php8/php-fpm.d/www.conf
