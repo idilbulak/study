37be4ed1d7a808d31c09124b82616c86105a5c21
Idil Bulak <ibulak@f0r6s4.codam.nl>
2023-04-20 14:04:10 +0200

    root login to mariadb to user login

--- srcs/requirements/mariadb/tools/create_database.sh
+++ srcs/requirements/mariadb/tools/create_database.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
-service mysql start
+mysql_install_db --user=mysql --datadir=/var/lib/mysql
 
-mysql -e "CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\`;"
-mysql -e "CREATE USER IF NOT EXISTS \`${DB_USER}\`@'localhost' IDENTIFIED BY '${DB_PASSWORD}';"
-mysql -e "GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO \`${DB_USER}\`@'%' IDENTIFIED BY '${DB_PASSWORD}';"
-mysql -e "ALTER USER \`${DB_ROOT}\@'localhost' IDENTIFIED BY '${DB_PASSWORD}';"
-mysql -e "FLUSH PRIVILEGES;"
+mysqld --user=mysql --skip-networking &
 
-mysqladmin -u root -p$DB_PASSWORD shutdown
+mysql -u root -e "CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\`;"
+mysql -u root -e "CREATE USER IF NOT EXISTS \`${DB_USER}\`@'localhost' IDENTIFIED BY '${DB_PASSWORD}';"
+mysql -u root -e "GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO \`${DB_USER}\`@'%' IDENTIFIED BY '${DB_PASSWORD}';"
+mysql -u root -e "ALTER USER \`${DB_ROOT}\@'localhost' IDENTIFIED BY '${DB_PASSWORD}';"
+mysql -u root -e "FLUSH PRIVILEGES;"
+
+mysqladmin -u root shutdown
 
 exec "$@"
