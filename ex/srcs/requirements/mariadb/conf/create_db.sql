b187d707411d436345a531fe1dcdb271378ddd7d
idil bulak <ib@idils-MacBook-Pro-2.local>
2023-04-25 20:34:45 +0200

    ex

--- ex/srcs/requirements/mariadb/conf/create_db.sql
+++
@@ -1,8 +0,0 @@
-CREATE DATABASE $MARIADB_DB;
-CREATE USER '$MARIADB_USER'@'%' IDENTIFIED by '$MARIADB_PWD';
-GRANT ALL PRIVILEGES ON $MARIADB_DB.* TO $MARIADB_USER@'%';
-
-/* Clears the priviliege cache on the MariaDB database for the changes to take effect*/
-FLUSH PRIVILEGES;
-
-SET PASSWORD FOR 'root'@'localhost' = PASSWORD('$MARIADB_ROOT_PWD');
