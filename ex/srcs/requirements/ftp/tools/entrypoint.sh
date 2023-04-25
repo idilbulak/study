b6c1c1d5963d97a5a6479f90174238a226e5f8eb
idil bulak <ib@idils-MacBook-Pro-2.local>
2023-04-25 21:26:07 +0200

    x

--- ex/srcs/requirements/ftp/tools/entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-echo www-data:$FTP_WWW_DATA_PASSWORD | chpasswd
-chown -R 82:82 /var/www/html
-
-exec "$@"
--- ex/srcs/requirements/redis/tools/entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-envsubst '${REDIS_PASSWORD}' </conf/redis.conf.template >/conf/redis.conf
-
-exec "$@"
